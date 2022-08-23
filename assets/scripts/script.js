d7a3d33170d7ed9c7f114fc31be7d6df4258551e
João Afrânio <joaoafraniorosa@hotmail.com>
2022-08-23 18:37:15 -0300

    adding check contact/privacy

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,45 @@
+const nav = document.querySelector("nav");
+
 function hiddenElement(element) {
   element.classList.add("hidden");
 }
 
 function showNav() {
-  const nav = document.querySelector("nav");
   nav.classList.remove("hidden");
+}
+
+function hiddenNav() {
+  nav.classList.add("hidden");
+}
+
+function login() {
+  const login = document.querySelector(".login");
+  login.classList.add("hidden");
+
+  const main = document.querySelector("main");
+  main.classList.remove("hidden");
+}
+
+function selectContact(contact) {
+  const previousSelected = document.querySelector(
+    ".contact .checkmark.selected"
+  );
+  if (previousSelected != null) {
+    previousSelected.classList.remove("selected");
+  }
+
+  const selected = contact.querySelector("ion-icon.checkmark");
+  selected.classList.add("selected");
+}
+
+function selectPrivacy(privacy) {
+  const previousSelected = document.querySelector(
+    ".privacy .checkmark.selected"
+  );
+
+  if (previousSelected != null) {
+    previousSelected.classList.remove("selected");
+  }
+  const selected = privacy.querySelector("ion-icon.checkmark");
+  selected.classList.add("selected");
 }
